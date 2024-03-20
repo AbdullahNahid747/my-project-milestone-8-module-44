c8311edfa333e37ab574a2a63cf89756103efb8c
Abdullahnahid <abdullahalnahid747@gmail.com>
2024-03-20 14:02:42 +0600

    added file

---
+++ src/Components/Navigationbar.jsx
@@ -0,0 +1,25 @@
+
+const Navigationbar = () => {
+
+
+    const routes = [
+        { id: 1, path: '/', name: 'Home' },
+        { id: 2, path: '/about', name: 'About' },
+        { id: 3, path: '/services', name: 'Services' },
+        { id: 4, path: '/contact', name: 'Contact' },
+        { id: 5, path: '*', name: 'NotFound' }
+      ];      
+
+
+    return (
+        <nav>
+            <ul className="md:flex">
+                {
+                    routes.map(route => <li className=" mr-4" key={route.id}><a href={route.path}></a>{route.name}</li>)  
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigationbar;
